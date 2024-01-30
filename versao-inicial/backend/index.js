eac30295d0da8b7c3b0b35edafcd97a675dd6f8a
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-01-30 17:16:31 +0000

    first

---
+++ versao-inicial/backend/index.js
@@ -0,0 +1,13 @@
+const app = require("express")();
+const consign = require("consign");
+
+consign()
+  .then("./config/middlewares.js")
+  .then("./api")
+  .then("./schedule")
+  .then("./config/routes.js")
+  .into(app);
+
+app.listen(3000, () => {
+  console.log("Backend INICIAL executando...");
+});
