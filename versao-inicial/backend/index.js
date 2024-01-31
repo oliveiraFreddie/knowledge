8d089126cd5e97d0323e1e73709b32d0bae53786
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-01-31 10:28:49 +0000

    API-usuario

--- versao-inicial/backend/index.js
+++ versao-inicial/backend/index.js
@@ -5,11 +5,12 @@ const db = require('./config/db')
 app.db = db
 
 consign()
-  .then("./config/middlewares.js")
-  .then("./api")
-  .then("./config/routes.js")
-  .into(app);
+    .then("./config/middlewares.js")
+    .then('./api/validation.js')
+    .then("./api")
+    .then("./config/routes.js")
+    .into(app);
 
 app.listen(3000, () => {
-  console.log("Backend INICIAL executando...");
+    console.log("Backend INICIAL executando...");
 });
