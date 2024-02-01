0ab9dde8fd88e161d76567f987bbbc134abe7f40
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-02-01 11:30:07 +0000

    api-estatisticas-mongoDB

---
+++ versao-inicial/backend/config/mongodb.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017";
+mongoose
+    .connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((e) => {
+        const msg = "ERRO! Não foi possível conectar com o MongoDB no projeto Inicial!";
+        console.log("\x1b[41m%s\x1b[37m", msg, "\x1b[0m");
+    });
+
