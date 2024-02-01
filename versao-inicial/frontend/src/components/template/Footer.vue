b11effbe6b207b932a109f0192c01057765ed647
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-02-01 15:39:43 +0000

    frontend-create-template-components

---
+++ versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="footer">
+        <span>Copyright <strong>Fr3D</strong> 2024</span>
+  </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+    .footer {
+        grid-area: footer;
+        background-color: #DDD;
+
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-right: 10px;
+    }
+</style>
