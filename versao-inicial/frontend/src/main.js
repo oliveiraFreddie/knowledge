08fcf44fdd528c31b682b284c6b798f6a79c0dbe
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-02-05 11:53:37 +0000

    article-item

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -11,7 +11,7 @@ import router from './config/router'
 Vue.config.productionTip = false
 
 //  TEMPORÁRIO
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkZyZWRlcmljbyBTYW50b3MgQWx0ZXJhZG8gcHJhIEFETSIsImVtYWlsIjoiZnJlZGRpZS5vbGl2ZWlyYTk1QGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE3MDY4MTk1MDIsImV4cCI6MTcwNzA3ODcwMn0.R1wBHrrikXUgtLalgEbmXA8rGPxoSlP_kjRvi6n1K8c'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6IkZyZWRlcmljbyBTYW50b3MgQWx0ZXJhZG8gcHJhIEFETSIsImVtYWlsIjoiZnJlZGRpZS5vbGl2ZWlyYTk1QGdtYWlsLmNvbSIsImFkbWluIjp0cnVlLCJpYXQiOjE3MDcxMzI3OTMsImV4cCI6MTcwNzM5MTk5M30.OjWDtI7QxV1gsAI74pP6fisJRCn_o1xBceNCuzQTRd8'
 
 new Vue({
   store,
