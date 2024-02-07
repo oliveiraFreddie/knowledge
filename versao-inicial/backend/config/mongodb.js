b679eecf97282c9bc086c9b8af510271ea3981e7
Frederico Oliveira <freddie.oliveira95@gmail.com>
2024-02-07 18:21:34 +0000

    mongodb-atlas

--- versao-inicial/backend/config/mongodb.js
+++ versao-inicial/backend/config/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const url = "mongodb://127.0.0.1:27017";
+const uri = "mongodb+srv://mongodb:1995@knowledge.cud38nd.mongodb.net/?retryWrites=true&w=majority";
 mongoose
-    .connect(url, {
+    .connect(uri, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
